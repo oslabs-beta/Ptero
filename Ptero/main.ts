d130f01c618beea40c2875e4898a59f0260594c9
Q <roussetquent1@gmail.com>
2021-10-04 14:54:01 -0400

    Setup of Ptero and TestAPI

--- Ptero/main.ts
+++ Ptero/main.ts
@@ -2,7 +2,7 @@ import { Application } from "https://deno.land/x/abc@v1.0.0-rc2/mod.ts";
 import "https://deno.land/x/dotenv/load.ts";
 //import router
 
-import { ErrorMiddleware, LogMiddleware } from "./utils/middlewares.ts";
+import { ErrorMiddleware } from "./utils/middlewares.ts";
 
 const app = new Application();
 //route anything OK to router
@@ -10,3 +10,6 @@ const app = new Application();
 //route 404 to 404 not found
 
 console.log(`server listening on http://localhost:5000`);
+
+//TO READ:
+//https://deno.land/manual@v1.14.2/examples/http_server
