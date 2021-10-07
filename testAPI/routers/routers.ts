b0435d515e63429485f04f8e27d666adb3ec249b
rheed14 <drhee11@gmail.com>
2021-10-06 20:23:04 -0400

    testapi finally working

--- testAPI/routers/routers.ts
+++ testAPI/routers/routers.ts
@@ -1,1 +1,24 @@
-//router file
+import { Router } from "https://deno.land/x/oak/mod.ts";
+
+import {
+  getCharacters,
+  addCharacter,
+} from "../controllers/controllers.ts";
+
+const testRouter = new Router();
+
+testRouter.get("/", ctx => {
+  ctx.response.body = "hello ptero";
+})
+
+testRouter.get("/characters", async(ctx: any, next: any) => {
+  console.log("we are in getCharacter route")
+  await getCharacters(ctx, next);
+  console.log("after getCharacters");
+});
+// testRouter.get("/characters/:id", getCharacter);
+testRouter.post("/characters", addCharacter);
+// testRouter.put("/characters/:id", updateCharacter);
+// testRouter.delete("/characters/:id", deleteCharacter);
+
+export default testRouter;
---
+++ testAPI/types/types.ts
@@ -0,0 +1,11 @@
+export interface Starwars {
+  name: string,
+  classification: string,
+  average_height: string,
+  average_lifespan: string,
+  hair_colors: string,
+  skin_colors: string,
+  eye_colors: string,
+  language: string,
+  homeworld: string,
+}
