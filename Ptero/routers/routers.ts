eb625def1331845cc49d07c5216d8f3816103fc0
rheed14 <drhee11@gmail.com>
2021-10-12 10:38:36 -0400

    caching user before going into different endpoints

--- Ptero/routers/routers.ts
+++ Ptero/routers/routers.ts
@@ -12,42 +12,27 @@ import { delay } from "https://deno.land/std/async/mod.ts";
 import { logData } from '../utils/dataLogging.ts'
 
 const pteroRouter = new Router();
+pteroRouter.use("/", async(ctx:any, next:any) => {
+  await cachingUser(ctx)
+  if(ctx.response.status === 401) return;
+  await next()
+});
 
 pteroRouter.get("/dinosaurs", async(ctx:any, next:any) => {
-  await cachingUser(ctx);
-  
-  if(ctx.response.status === 401) return;
-
   await caching(ctx, getDinosaurs);
 });
 
 pteroRouter.get("/dinosaurs/:id", async(ctx:any, next:any) => {
-  await cachingUser(ctx);
-
-  if(ctx.response.status === 401) return;
-
   await caching(ctx, getDinosaur);
 });
 
 pteroRouter.post("/dinosaurs", async(ctx:any, next:any) => {
-  await cachingUser(ctx);
-
-  if(ctx.response.status === 401) return;
-
   await addDinosaur;
 });
 pteroRouter.put("/dinosaurs/:id", async(ctx:any, next:any) => {
-  await cachingUser(ctx);
-
-  if(ctx.response.status === 401) return;
-
   await caching(ctx, updateDinosaur);
 });
 pteroRouter.delete("/dinosaurs/:id", async(ctx:any, next:any) => {
-  await cachingUser(ctx);
-
-  if(ctx.response.status === 401) return;
-  
   await deleteDinosaur;
 });
 
