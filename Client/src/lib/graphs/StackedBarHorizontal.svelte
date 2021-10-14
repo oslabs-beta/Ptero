7d4f36fa4a0267f5c090153199c7a270b3f7d05f
Q <roussetquent1@gmail.com>
2021-10-14 08:07:41 -0400

    working data processing for stacked bars

--- Client/src/lib/graphs/StackedBarHorizontal.svelte
+++ Client/src/lib/graphs/StackedBarHorizontal.svelte
@@ -1,46 +1,47 @@
 <script>
 	import * as Pancake from '@sveltejs/pancake';
-
-	export const totals = [
-		{ route: '/test', GET: 3840, POST: 1920, PUT: 960, DELETE: 400, id: 1 },
-		{ route: '/test/api', GET: 1600, POST: 1440, PUT: 960, DELETE: 400, id: 2 },
-		{ route: '/api', GET: 820, POST: 1000, PUT: 640, DELETE: 400, id: 3 },
-		{ route: '/Dinosaurs', GET: 820, POST: 560, PUT: 720, DELETE: 400, id: 4 },
-		{ route: '/banana', GET: 3840, POST: 1920, PUT: 960, DELETE: 400, id: 5 },
-		{ route: '/test/api', GET: 1600, POST: 1440, PUT: 960, DELETE: 400, id: 6 },
-		{ route: '/api', GET: 820, POST: 1000, PUT: 640, DELETE: 400, id: 7 },
-		{ route: '/Dinosaurs', GET: 820, POST: 560, PUT: 720, DELETE: 400, id: 8 }
-	];
+	import { IndexBars } from '$lib/store.ts';
+	//Works with const but doesn't re-render as it cannot be modified but does not work with let as it is empty at some point. look up about Svelte and placeholders.
 
 	export const method = ['GET', 'POST', 'PUT', 'DELETE'];
 	export const colors = ['lightgreen', 'yellow', 'orange', 'red'];
+	let stacks;
+	let max;
 
-	const stacks = Pancake.stacks(totals, method, 'id');
-	console.log(stacks[0]);
+	let totals = [];
 
-	const max = stacks.reduce((max, stack) => Math.max(max, ...stack.values.map((v) => v.end)), 0);
+	IndexBars.subscribe((value) => {
+		totals = value.slice(0, 10);
+		console.log(totals);
+		stacks = Pancake.stacks(totals, method, 'id');
+		max = stacks.reduce((max, stack) => Math.max(max, ...stack.values.map((v) => v.end)), 0);
+	});
 </script>
 
-<div class="chart">
-	<Pancake.Chart x1={0} x2={max} y1={0.5} y2={totals[totals.length - 1]['id'] + 0.5}>
-		<Pancake.Grid horizontal count={totals[totals.length - 1]['id']} let:value let:first>
-			<div class="grid-line horizontal"><span>{totals[value - 1]['route']}</span></div>
-		</Pancake.Grid>
+{#if $IndexBars.length > 1}
+	<div class="chart">
+		<Pancake.Chart x1={0} x2={max} y1={0.5} y2={5.5}>
+			<Pancake.Grid horizontal count={totals.length} let:value let:first>
+				{#if totals[value - 1]}
+					<div class="grid-line horizontal"><span>{totals[value - 1]['route']}</span></div>
+				{/if}
+			</Pancake.Grid>
 
-		<Pancake.Grid vertical count={5} let:value>
-			<div class="grid-line vertical" />
-			<span class="x-label">{value}</span>
-		</Pancake.Grid>
+			<Pancake.Grid vertical count={8} let:value>
+				<div class="grid-line vertical" />
+				<span class="x-label">{value}</span>
+			</Pancake.Grid>
 
-		{#each stacks as stack, i}
-			{#each stack.values as d}
-				<Pancake.Box x1={d.start} x2={d.end} y1={d.i - 0.5} y2={d.i + 0.5}>
-					<div class="box" style="background-color: {colors[i]}" />
-				</Pancake.Box>
+			{#each stacks as stack, i}
+				{#each stack.values as d}
+					<Pancake.Box x1={d.start} x2={d.end} y1={d.i - 0.5} y2={d.i + 0.5}>
+						<div class="box" style="background-color: {colors[i]}" />
+					</Pancake.Box>
+				{/each}
 			{/each}
-		{/each}
-	</Pancake.Chart>
-</div>
+		</Pancake.Chart>
+	</div>
+{/if}
 
 <style>
 	.chart {
