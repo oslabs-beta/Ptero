46708a1303998fcdf1042e74504a6135ca840d56
Q <roussetquent1@gmail.com>
2021-10-28 13:38:31 -0400

    merged electron and full project

---
+++ Client/src/electron.cjs
@@ -0,0 +1,112 @@
+const windowStateManager = require('electron-window-state');
+const contextMenu = require('electron-context-menu');
+const { app, BrowserWindow } = require('electron');
+const serve = require('electron-serve');
+const { fork } = require('child_process');
+
+// require('../.svelte-kit/build/app');
+// const exec = require('child_process').exec;
+// const cmd2 = 'npm run svelte-kit dev';
+
+try {
+	require('electron-reloader')(module);
+} catch (e) {
+	console.error(e);
+}
+
+const serveURL = serve({ directory: '.' });
+const port = process.env.PORT || 3000;
+// = 3000;
+const dev = !app.isPackaged;
+let mainWindow;
+
+function createWindow() {
+	let windowState = windowStateManager({
+		defaultWidth: 1920,
+		defaultHeight: 1080
+	});
+
+	const mainWindow = new BrowserWindow({
+		backgroundColor: 'whitesmoke',
+		titleBarStyle: 'hidden',
+		autoHideMenuBar: false, //it was true
+		trafficLightPosition: {
+			x: 17,
+			y: 32
+		},
+		minHeight: 450,
+		minWidth: 500,
+		webPreferences: {
+			enableRemoteModule: true,
+			contextIsolation: true, // it was true
+			nodeIntegration: true,
+			spellcheck: false,
+			devTools: true
+		},
+		x: windowState.x,
+		y: windowState.y,
+		width: windowState.width,
+		height: windowState.height
+	});
+
+	windowState.manage(mainWindow);
+
+	mainWindow.once('ready-to-show', () => {
+		mainWindow.show();
+		mainWindow.focus();
+	});
+
+	mainWindow.on('close', () => {
+		windowState.saveState(mainWindow);
+	});
+
+	return mainWindow;
+}
+
+contextMenu({
+	showLookUpSelection: false,
+	showSearchWithGoogle: true,
+	showCopyImage: false,
+	prepend: (defaultActions, params, browserWindow) => [
+		{
+			label: 'Make App 💻'
+		}
+	]
+});
+
+function loadVite(port) {
+	mainWindow.loadURL(`http://localhost:${port}`).catch((e) => {
+		console.log('Error loading URL, retrying', e);
+		setTimeout(() => {
+			loadVite(port);
+		}, 200);
+	});
+}
+
+function createMainWindow() {
+	mainWindow = createWindow();
+	mainWindow.once('close', () => {
+		mainWindow = null;
+	});
+
+	if (dev) loadVite(port);
+	else {
+		serveURL(mainWindow);
+		ssr = fork(`${__dirname}/index.js`);
+	}
+}
+
+app.on('ready', createMainWindow);
+app.on('activate', () => {
+	if (!mainWindow) {
+		createMainWindow();
+	}
+});
+app.on('ready', function () {
+	mainWindow = new BrowserWindow({ width: 800, height: 600 });
+	mainWindow.webContents.openDevTools();
+});
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') app.quit();
+});
--- Client/svelte.config.js
+++ Client/svelte.config.js
@@ -2,23 +2,23 @@ import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-node';
 // import pkg from './package.json';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://github.com/sveltejs/svelte-preprocess
+// 	// for more information about preprocessors
+// 	preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+// 	kit: {
+// 		// hydrate the <div id="svelte"> element in src/app.html
+// 		target: '#svelte',
 
-		// vite: {
-		// 	ssr: {
-		// 		// external: Object.keys(pkg.Dependencies || {})
-		// 	}
-		// }
-	}
-};
+// 		// vite: {
+// 		// 	ssr: {
+// 		// 		// external: Object.keys(pkg.Dependencies || {})
+// 		// 	}
+// 		// }
+// 	}
+// };
 
 export default {
 	kit: {
